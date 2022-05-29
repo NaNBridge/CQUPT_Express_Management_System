a2008c2dd6a1241d00682ddb85960ef922b172b0
NaNBridge <lightwingfsade@gmail.com>
2022-05-29 13:42:30 +0800

    创建了数据库

--- MySQL_query/CQUPTEMS_data_operation.sql
+++ MySQL_query/CQUPTEMS_data_operation.sql
@@ -1,5 +1,30 @@
 -- 创建数据库
-create database CQUPTEMS_DB;
+create database cquptems_db;
 -- 创建表
+use cquptems_db;
+-- 创建快递信息表
+create table Packages(
+    package_ID varchar(50) not null ,
+    package_owner_phone_number decimal(11,0) not null ,
+    shipping_address varchar(100)not null,
+    express_company varchar(100)not null,
+    primary key (package_ID,package_owner_phone_number)
+);
+-- 床架快递驿站表
+create table Express_Station(
+    package_ID varchar(50) not null ,
+    pickup_user_student_ID decimal(10,0) not null ,
+    import_time timestamp not null ,
+    export_time timestamp default null,
+    error_status int default 0,
+    primary key (package_ID,pickup_user_student_ID)
+);
 
-create table staion
+-- 创建用户表
+create table Users(
+    user_name varchar(20) not null ,
+    user_student_ID decimal(10,0) not null unique ,
+    user_phone_number decimal(11,0)not null unique ,
+    unique_pickup_code varchar(50) not null unique ,
+    primary key (user_name,user_phone_number,user_student_ID,unique_pickup_code)
+)
